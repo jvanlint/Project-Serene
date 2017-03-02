a5823b68e62e1d634bd6ce24c34cfc52d9339aec
Jason van Lint <jvanlint@mac.com>
2017-03-02 12:39:13 +1100

    App icons for iPhone added.

---
+++ Project Serene/Project Serene/Controllers/PlanTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PlanTableViewCell.swift
+//  Project Serene
+//
+//  Created by Jason van Lint on 2/3/17.
+//  Copyright © 2017 TheSwiftCollective. All rights reserved.
+//
+
+import UIKit
+
+class PlanTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
