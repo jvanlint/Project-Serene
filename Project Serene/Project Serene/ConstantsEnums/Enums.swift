335efc681977185c270917241cee64acb4409158
Jason van Lint <jvanlint@mac.com>
2017-03-03 11:15:43 +1100

    lower res iPad icon added enums and constants folder created enums file added to store global enums for the app. Have added some samples.

---
+++ Project Serene/Project Serene/ConstantsEnums/Enums.swift
@@ -0,0 +1,28 @@
+//
+//  Enums.swift
+//  Project Serene
+//
+//  Created by Jason van Lint on 3/3/17.
+//  Copyright © 2017 TheSwiftCollective. All rights reserved.
+//
+
+enum ProcedureType {    
+    case portAccess
+    case IVInsertion
+    case hickmanDressing
+    case bloodCollection
+}
+
+enum Companion{
+    case mum
+    case dad
+    case grandpa
+    case grandma
+    case brother
+    case sister
+    case friend
+    case toy
+    case aunt
+    case uncle
+}
+
